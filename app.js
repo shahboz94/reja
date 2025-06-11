518fba97d8a153cfe0ffe134ccacd2233b05efd3
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2025-06-12 02:26:49 +0900

    feat:delete contains

--- app.js
+++ app.js
@@ -1,45 +1,37 @@
-console.log("Web Serverni boshlash");
 const express = require("express");
-const res = require("express/lib/response");
+const { ObjectId } = require("mongodb"); // <<< BU YERDA KERAKLI QATOR
 const app = express();
-// const fs = require("fs");
 
-// let user;
-// fs.readFile("database/user.json", "utf8", (err, data) => {
-//   if (err) {
-//     console.log("ERROR:", err);
-//   } else {
-//     user = JSON.parse(data);
-//   }
-// });
-
-//MongoDB chaqiramiz
 const db = require("./server").db();
-/*ushbu ozgaruvchi mongodb instanceni qolga olib beradi
-bu orqali databasega turli malumotlarni
-yozish/ochirish/oqish... */
-//1.Kirish code
+
+// Middleware
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//2. Session codes
-//3. Views codes
+// View sozlamalari
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-//4.Routing codes
+// Routing
 app.post("/create-item", (req, res) => {
-  console.log("user entered /create-item");
-  console.log(req.body);
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    console.log(data.ops);
     res.json(data.ops[0]);
   });
 });
+
+app.post("/delete-item", (req, res) => {
+  const id = req.body.id;
+  db.collection("plans").deleteOne(
+    { _id: new ObjectId(id) }, // <<< ObjectId to'g'ri ishlatildi
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
+});
+
 app.get("/", function (req, res) {
-  console.log("user entered /");
   db.collection("plans")
     .find()
     .toArray((err, data) => {
@@ -47,13 +39,13 @@ app.get("/", function (req, res) {
         console.log(err);
         res.end("something went wrong");
       } else {
-        console.log(data);
         res.render("reja", { items: data });
       }
     });
 });
+
 app.get("/author", (req, res) => {
-  res.render("author", { user: user });
+  res.render("author", { user: user }); // "user" o'zgaruvchisi ham e'lon qilinmagan bo'lishi mumkin!
 });
 
 module.exports = app;
--- public/browser.js
+++ public/browser.js
@@ -40,3 +40,26 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
       console.log("Iltimos qaytadan harakat qiling!");
     });
 });
+
+document.addEventListener("click", function (e) {
+  //   delete opere
+  console.log(e.target);
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Aniq ochirmoqchimisiz?")) {
+      axios
+        .post("/delete-item", { id: e.target.getAttribute("data-id") })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.remove();
+        })
+        .catch((err) => {
+          console.log("Iltimos qaytadan harakat qiling!");
+        });
+    }
+  }
+
+  //  edit oper
+  if (e.target.classList.contains("edit-me")) {
+    alert("siz edit tugmasini bosdingiz");
+  }
+});
